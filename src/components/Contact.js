d5cdba7bc6863437907fa9a26eb1a2a13e9700ab
AdelaideChoveau <adelaide.choveau@gmail.com>
2021-10-12 21:30:05 +0200

    Coontact and emailJs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,30 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import emailjs from 'emailjs-com';
 import { Icon } from '@iconify/react'
-import locationIcon from '@iconify/icons-mdi/map-marker'
+import heartIcon from '@iconify/icons-mdi/heart-settings'
+
+const Result = () => {
+    return(
+        <p>Your message has been successfully sent 👋</p>
+    )
+}
 
 const Contact = () => {
+    const [result,showResult] = useState(false);
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_5z1b1ds', 'template_dvyxjse', e.target, 'user_i4hnuLomknEZTRZO15lu7')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+        e.target.reset();
+        showResult(true);
+    };
+
+
+
     return (
 
 <div class="contactme" id="contact">
     <div class="contactOverlay">
         <div class="container">
-
-
-
-            <form action="" onSubmit="">
+            <form action="" onSubmit={sendEmail}>
                 <div class="formWord">
-                     <h2>You want to support me, contribute or just leave a message !</h2>
+                     <h2><Icon icon={heartIcon} className="heart-icon" ></Icon>You can support us or leave a message !</h2>
                         <span>Full Name</span>
                      <br />
-                        <input class="input100" type="text" name="fullName" required />
-                    <br />
-                        <span>Phone Number</span>
-                    <br />
-                        <input class="input100" type="text" name="phone" required />
+                        <input class="nameInput" type="text" name="fullName" required />
                     <br />
                         <span>Enter Email</span>
                     <br />
-                        <input class="input100" type="text" name="email" required />
+                        <input class="emailInput" type="text" name="email" required />
                     <br />
                 </div>
     
@@ -34,7 +51,7 @@ const Contact = () => {
                         <textarea name="message" required></textarea>
                     <br />
                         <button>Share the love</button>
-                            <div class="row">All Done</div>
+                            <div class="row">{result ? <Result/> : null}</div>
                 </div>
             </form>
         </div>
